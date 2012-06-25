6d48198215216486add04f351db8991d844a662d
scompton <scompton@>
2012-06-25 16:12:31 -0600

    working

--- IbmIeeeAmplitudes.java
+++ IbmIeeeAmplitudes.java
@@ -1,8 +1,11 @@
+package segy;
+
 import java.util.logging.Logger;
 
 import edu.mines.jtk.dsp.Fft;
 import edu.mines.jtk.dsp.Real1;
 import edu.mines.jtk.dsp.Sampling;
+import edu.mines.jtk.la.DMatrix;
 import edu.mines.jtk.util.ArrayMath;
 import edu.mines.jtk.util.Cdouble;
 
@@ -35,6 +38,10 @@ public class IbmIeeeAmplitudes {
 		return amplitudes;
 	}
 	
+	public static Sampling getSampling(float[] f) {
+		return new Fft(f).getFrequencySampling1();
+	}
+	
 	/**
 	 * Computes the ratio of a weighted average of high
 	 * frequencies to a weighted average of low frequencies
@@ -42,7 +49,7 @@ public class IbmIeeeAmplitudes {
 	 * @param a the array of amplitudes
 	 * @return the ratio of high to low frequencies
 	 */
-	public static float computeRatio(float[] a) {
+	public static float computeLinearRatio(float[] a, float[] highW, float[] lowW) {
 		int len = a.length;
 		float hSum  = 0;
 		float lSum  = 0;
@@ -52,10 +59,16 @@ public class IbmIeeeAmplitudes {
 			float hw = (float)i / (float)(len-1);
 			hwSum += hw;
 			hSum  += hw*a[i];
+			if (highW != null) {
+				highW[i] = hw;
+			}
 			
 			float lw = (float)(len-1-i) / (float)(len-1);
 			lwSum += lw;
 			lSum  += lw*a[i];
+			if (lowW != null) {
+				lowW[i] = lw;
+			}
 		}
 		float h = hSum/hwSum;
 		float l = lSum/lwSum;
@@ -64,4 +77,83 @@ public class IbmIeeeAmplitudes {
 		return r;
 	}
 	
+	public static float computeSCRatio(float[] a, float[] highW, float[] lowW) {
+		int len = a.length;
+		float hSum  = 0;
+		float lSum  = 0;
+		float hwSum = 0;
+		float lwSum = 0;
+		for (int i = 0; i < len; i++) {
+			float radians = (float)Math.toRadians((float)i / (float)(len-1) * 90f);
+			float hw = (float)Math.sin(radians);
+			hwSum += hw;
+			hSum  += hw*a[i];
+			if (highW != null) {
+				highW[i] = hw;
+			}
+			
+			float lw = (float)Math.cos(radians);
+			lwSum += lw;
+			lSum  += lw*a[i];
+			if (lowW != null) {
+				lowW[i] = lw;
+			}
+		}
+		float h = hSum/hwSum;
+		float l = lSum/lwSum;
+		float r = h/l;
+		LOG.info("h="+h+", l="+l+", r="+r);
+		return r;
+	}
+	
+	public static float computeRatio(float[] a, float p, float n,
+			float[] highW, float[] lowW) {
+		int len = a.length;
+		float hSum  = 0;
+		float lSum  = 0;
+		float wSum = 0;
+		for (int i = 0; i < len; i++) {
+			float x = (float)i / (float)(len-1);
+			float w = getWeight(x, p, n);
+			wSum += w;
+			hSum  += w*a[i];
+			lSum  += w*a[(len-1)-i];
+			if (highW != null) {
+				highW[i] = w;
+			}
+			if (lowW != null) {
+				lowW[len-1-i] = w;
+			}
+		}
+		float h = hSum/wSum;
+		float l = lSum/wSum;
+		float r = h/l;
+		LOG.info("h="+h+", l="+l+", r="+r);
+		return r;
+	}
+	
+	public static Real1[] getWeights(Sampling s, float p, float n) {
+		int nk = s.getCount();
+		float[] hwArray = new float[nk];
+		float[] lwArray = new float[nk];
+		for (int i = 0; i < nk; i++) {
+			float x = (float)i / (float)(nk-1);
+			float w = getWeight(x, p, n);
+			hwArray[i] = w;
+			lwArray[(nk-1)-i] = w;
+		}
+		Real1[] weights = {new Real1(s, hwArray), new Real1(s, lwArray)};
+		return weights;
+	}
+	
+	private static float getWeight(float x, float slope, float power) {
+		float w;
+		if (5*x < slope) {
+			w = (float)((3*Math.pow(5*x/slope, 8) - 2*Math.pow(5*x/slope, 12))
+					* Math.pow(x, power));
+		} else {
+			w = (float)Math.pow(x, power);
+		}
+		return w;
+	}
 }
--- jython/ibmvieee.py
+++ jython/ibmvieee.py
@@ -1,20 +1,34 @@
 from imports import *
-import DoFft
 
 ################################################################################
 
 def main(args):
-  basedir = "/Users/scompton/opendtect/F3_Demo/Seismics/"
-  sgyfile = basedir+"f3d.sgy"
+  basedir = "/disk2/code/git/smc/"
+  sgyfile = basedir+"f3d11lines.sgy"
   si = SegyImage(sgyfile)
   fibm,fieee = getTraces(si)
-  plotIbmIeeeFloats(fibm,fieee)
+  sampling = IbmIeeeAmplitudes.getSampling(fibm)
+  n1 = len(fibm)
+  #plotIbmIeeeFloats(fibm,fieee)
+  p=3.8
+  n=0.2
+  #testWeights(p,n)
   db = True;
-  gibm,aibm = doFft(fibm,db)
-  gieee,aieee = doFft(fieee,db)
-  ribm = computeAvg(aibm.getValues(), "IBM")
-  rieee = computeAvg(aieee.getValues(), "IEEE")
-  plotFft(gibm,gieee,aibm,aieee,ribm,rieee)
+  aibm = IbmIeeeAmplitudes.computeAmplitudes(fibm,db)
+  aieee = IbmIeeeAmplitudes.computeAmplitudes(fieee,db)
+
+  hw = zerofloat(n1)
+  rhw = Real1(sampling, hw)
+  lw = zerofloat(n1)
+  rlw = Real1(sampling,
+  ribm = IbmIeeeAmplitudes.computeLinearRatio(aibm.getValues(),hw,lw)
+  rieee = IbmIeeeAmplitudes.computeLinearRatio(aieee.getValues(),None,None)
+  plotFft(aibm,aieee,ribm,rieee,hw,lw)
+  #ribm = IbmIeeeAmplitudes.computeSCRatio(aibm.getValues())
+  #rieee = IbmIeeeAmplitudes.computeSCRatio(aieee.getValues())
+  #ribm = IbmIeeeAmplitudes.computeRatio(aibm.getValues(),p,n)
+  #rieee = IbmIeeeAmplitudes.computeRatio(aieee.getValues(),p,n)
+  #plotFft(aibm,aieee,ribm,rieee)
 
 def getTraces(si):
   ntrace = si.countTraces()
@@ -27,35 +41,39 @@ def getTraces(si):
   si.setFormat(fmt)
   return fibm, fieee
 
-def doFft(f, db):
-  fft = DoFft(f, db)
-  g = fft.getG()
-  a = fft.getAmps()
-  return g,a
+def testWeights(p,n):
+  #w1,w2 = [],[]
+  w1,w2 = IbmIeeeAmplitudes.getWeights(p,n)
+  #len = 501
+  #for i in range(len):
+    #d = (i/(len-1.0))*90.0
+    #w1.append(cos(d*ArrayMath.DBL_PI/180.0))
+    #w2.append(sin(d*ArrayMath.DBL_PI/180.0))
+    #x = i/(len-1.0)
+    #if (5*x < p):
+      #w = (3*pow(5*x/p, 2) - 2*pow(5*x/p, 3)) * pow(x, n)
+    #else:
+      #w = pow(x, n)
+    #print w,
+    #w1.append(w)
+  pp = PlotPanel()
+  pp.addPoints(w1)
+  pp.addPoints(w2)
+  pf = PlotFrame(pp)
+  pf.setVisible(True)
 
-def computeAvg(amplitudes, label):
-  h = DoFft.weightedAvg(amplitudes, True)
-  l = DoFft.weightedAvg(amplitudes, False)
-  r = h/l
-  print "%s: h=%f, l=%f, r=%f" % (label,h,l,r)
-  return r
-
-def plotFft(gibm, gieee, gkibm, gkieee, ribm, rieee):
-  pp1 = PlotPanel(2,1)
-  pp1.setTitle("Frequency - IBM (top) versus IEEE (bottom)")
-  pp1.addPoints(0,0,gibm)
-  pp1.addPoints(1,0,gieee)
-  pf1 = PlotFrame(pp1)
-  pf1.setSize(1000,800)
-  pf1.setVisible(True)
-
+def plotFft(aibm, aieee, ribm, rieee, hw, lw):
   pp2 = PlotPanel(2,1)
   pp2.setTitle("IBM (top r=%f) versus IEEE (bottom r=%f)" % (ribm,rieee))
   pp2.setHLabel(0,"Frequency (cycles/sample)")
   pp2.setVLabel(0,"IBM amplitude (dB)")
   pp2.setVLabel(1,"IEEE amplitude (dB)")
-  pp2.addPoints(0,0,gkibm.getSampling(),gkibm.getValues())
-  pp2.addPoints(1,0,gkieee.getSampling(),gkieee.getValues())
+  pp2.addPoints(0,0,aibm.getSampling(),aibm.getValues())
+  pp2.addPoints(0,0,hw)
+  pp2.addPoints(0,0,lw)
+  pp2.addPoints(1,0,aieee.getSampling(),aieee.getValues())
+  pp2.addPoints(1,0,hw)
+  pp2.addPoints(1,0,lw)
   pf2 = PlotFrame(pp2)
   pf2.setSize(1000,800)
   pf2.setVisible(True)
